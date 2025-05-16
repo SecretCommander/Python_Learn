b6444c7ab31b5224712d9303578bbfa9d6af51f4
SecretCommander <farrelfff3@gmail.com>
2025-05-16 09:40:02 +0700

    Day 1

---
+++ day_1/hellowprld.py
@@ -0,0 +1,25 @@
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+print("Farrel Ardan Awaly") #My name
+print("Dont Have") #My family name
+print("Indonesia") #My country
+print("I am enjoying 30 days of python")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(True))
+print(type({'name': 'Farrel', 'age': 23}))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type({'Farrel', 'Python', 'Indonesia'}))
+print(type(('Farrel', 'Python', 'Indonesia')))
+print(type('Farrel Ardan Awaly'))
+print(type('Dont Have'))
+print(type('Indonesia'))
