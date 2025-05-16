b17ca48884837d3537c8dbd78c91b96f6b855211
SecretCommander <farrelfff3@gmail.com>
2025-05-16 10:08:21 +0700

    Day 2

---
+++ day_2/variables.py
@@ -0,0 +1,44 @@
+# 30DaysOfPython
+first_name = 'Farrel'
+last_name = 'Ardan Awaly'
+full_name = first_name + ' ' + last_name
+country = 'Indonesia'
+city = "Medan"
+age = 19
+year = 2025
+is_married = False
+is_true = True
+is_light_on = True
+number, floating, complex = 10, 9.8, 3.14 - 4j
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(number))
+print(type(floating))
+print(type(complex))
+print(len(first_name))
+num_one, num_two = 5, 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+radius = 30
+area_of_circle = 3.14 * radius ** 2
+circum_of_circle = 2 * 3.14 * radius
+radius = input("Enter the radius of the circle: ")
+area_of_circle = 3.14 * float(radius) ** 2
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+country = input("Enter your country: ")
+age = input("Enter your age: ")
+help('keywords')
